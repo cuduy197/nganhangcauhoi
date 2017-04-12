be9257ebdda96abd58dc9c7be890a256302e2be4
Bùi Khương Duy <cuduy197@gmail.com>
2017-04-13 03:10:05 +0700

    :stars: Cập nhật nhỏ

--- src/main.js
+++ src/main.js
@@ -13,11 +13,11 @@ import 'material-design-lite/material.min.css';
 import './assets/custom.css';
 
 import App from './App';
-import { sync } from 'vuex-router-sync';
+//import { sync } from 'vuex-router-sync';
 import router from './router';
 import { store } from './store/index';
 
-sync(store, router); // done.
+//sync(store, router); // done.
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
