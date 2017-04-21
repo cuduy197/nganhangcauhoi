965f8f259d0019fe7e7463a55c00f6d89e4dfa1b
cuduy197 <cuduy197@gmail.com>
2017-04-21 18:17:22 +0700

    Cập nhật nhỏ :tada:

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 6 */ /*jshint -W030 */
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
@@ -7,8 +6,6 @@ import notFoundPage from '@/components/notFoundPage';
 import toan from '@/components/subjects/toan/toan';
 import van from '@/components/subjects/van/van';
 import anh from '@/components/subjects/anh/anh';
-
-
 
 Vue.use(Router);
 
