060dc22d59cb53c4c5040088b4904a29838c5d80
cuduy197 <cuduy197@gmail.com>
2017-05-10 14:47:14 +0700

    Cập nhật Travis CI :tada:

--- build/openUrl.js
+++ build/openUrl.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 6 */ /*jshint -W030 */
 const readline = require('readline');
 var ora = require('ora');
 var opn = require('opn');
@@ -7,7 +6,6 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
 rl.question('Bạn có muốn truy cập ứng dụng? (y/n):  ', (answer) => {
     if (answer == 'y' || answer == 'c') {
         var spinner = ora('Đang truy cập ứng dụng ...');
@@ -15,8 +13,8 @@ rl.question('Bạn có muốn truy cập ứng dụng? (y/n):  ', (answer) => {
         spinner.start();
         setTimeout(() => {
             spinner.succeed(['Đã truy cập ứng dụng']);
-            opn('http://nhch.surge.sh');
-        }, 1234);
+            opn('http://cauhoi.cunghoctot.net/');
+        }, 234);
         rl.close();
     } else {
         rl.close();
